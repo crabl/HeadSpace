be25b9910d57290ebe39ea298e33c2dd6b7f98ca
Chris Rabl <rabl.christopher@gmail.com>
2014-06-03 10:34:27 -0600

    Adding ctrl

--- hs-barber.py
+++ hs-barber.py
@@ -12,11 +12,9 @@ from headspace import *
 s = Server().boot()
 s.start()
 
-ui = HeadSpaceUI("Barbershop")
-
 shaverWav = SfPlayer("testfiles/shaver.wav", loop=True)
 shaver = HeadSpace(shaverWav, 0, 0).out()
-ui.add("Shaver", shaver)
+shaver.ctrl(title="Shaver")
 
 backgroundWav = SfPlayer("testfiles/VictorOrchestra-BlackWhiteRagtime.wav", loop=True)
 background = HeadSpace(backgroundWav, 100, 30)
@@ -27,4 +25,4 @@ dooropen = HeadSpace(dooropenWav, -20, -10)
 phoneringWav = SfPlayer("testfiles/phonering.wav", loop=True)
 phonering = HeadSpace(phoneringWav, -150, -20)
 
-ui.show()
+s.gui(locals())
